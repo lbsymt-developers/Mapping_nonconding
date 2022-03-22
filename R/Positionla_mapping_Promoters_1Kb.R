b49ef1ee4beea627351de68e34a3e922ce835733
Erickcufe <erickcufe94@gmail.com>
2022-03-22 14:02:05 -0600

    .

--- R/Positionla_mapping_Promoters_1Kb.R
+++ R/Positionla_mapping_Promoters_1Kb.R
@@ -8,3 +8,4 @@ mcols(credpromoter) <- cbind(mcols(credpromoter), mcols(promoterranges[subjectHi
 gen_anno <- vroom::vroom("https://www.genenames.org/cgi-bin/download/custom?col=gd_hgnc_id&col=gd_app_sym&col=gd_app_name&col=gd_status&col=gd_prev_sym&col=gd_aliases&col=gd_pub_chrom_map&col=gd_pub_acc_ids&col=gd_pub_refseq_ids&col=gd_pub_ensembl_id&status=Approved&status=Entry%20Withdrawn&hgnc_dbtag=on&order_by=gd_hgnc_id&format=text&submit=submit")
 gen_anno <- gen_anno[!is.na(gen_anno$`Ensembl gene ID`), ]
 save(gen_anno, file = "data/Gen_Anno.rda")
+
