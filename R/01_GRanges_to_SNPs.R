b7a707718aef932cc4a6401700e97ff1c151e618
Erickcufe <erickcufe94@gmail.com>
2022-03-22 13:57:23 -0600

    new data

--- R/01_GRanges_to_SNPs.R
+++ R/01_GRanges_to_SNPs.R
@@ -1,9 +1,43 @@
 library(GenomicRanges)
 options(stringsAsFactors = F)
-credSNP <- vroom::vroom("data/Supplementary_Table_8_Jansen.csv")
-credSNP <- credSNP[credSNP$`Credible Causal`=="Yes",]
 
-credranges <- GRanges(credSNP$Chr, IRanges(credSNP$bp, credSNP$bp), rsid = credSNP$SNP, P=credSNP$P)
+gwascatalog_AD <- seekerBio::seeker_gwas("Alzheimer")
+gwascatalog_AD <- gwascatalog_AD[gwascatalog_AD$P.VALUE <= 5E-8, ]
+coding <- c("missense_variant", "stop_gained", "synonymous_variant")
+`%notin%` <- Negate(`%in%`)
+gwascatalog_AD <- gwascatalog_AD[gwascatalog_AD$CONTEXT %notin% coding, ]
+which_ad <- stringr::str_detect(gwascatalog_AD$DISEASE.TRAIT, "Alzheimer")
+gwascatalog_AD <- gwascatalog_AD[which_ad, ]
+
+colnames(gwascatalog_AD)[29] <- "P"
+colnames(gwascatalog_AD)[23] <- "rsid"
+colnames(gwascatalog_AD)[14] <- "bp"
+colnames(gwascatalog_AD)[13] <- "Chr"
+
+gwascatalog <- gwascatalog_AD[, c("rsid", "Chr", "bp", "P")]
+readr::write_csv(gwascatalog, "data/tofix_gwascatalog.csv")
+gwascatalog <- readr::read_csv("data/tofix_gwascatalog.csv")
+
+Jansen_SNP <- vroom::vroom("data/Supplementary_Table_8_Jansen.csv")
+Jansen_SNP <- Jansen_SNP[Jansen_SNP$`Credible Causal`=="Yes",]
+Jansen_SNP <- Jansen_SNP[Jansen_SNP$P <= 5E-8,]
+colnames(Jansen_SNP)[2] <- "rsid"
+Jansen <-  Jansen_SNP[, c("rsid", "Chr", "bp", "P")]
+
+grasp_ad <- data.table::fread("data/GRASP_AD.csv")
+colnames(grasp_ad)[2] <- "rsid"
+colnames(grasp_ad)[8] <- "Chr"
+colnames(grasp_ad)[9] <- "bp"
+colnames(grasp_ad)[3] <- "P"
+table(grasp_ad$FunctionalClass)
+coding_grasp <- c("cds-synon", "missense")
+grasp_ad <- grasp_ad[grasp_ad$FunctionalClass %notin% coding_grasp, ]
+grasp <- grasp_ad[, c("rsid", "Chr", "bp", "P")]
+
+credSNP <- rbind(gwascatalog, grasp, Jansen)
+credSNP <- credSNP[!duplicated(credSNP$rsid), ]
+
+credranges <- GRanges(credSNP$Chr, IRanges(credSNP$bp, credSNP$bp), rsid = credSNP$rsid, P=credSNP$P)
 save(credranges, file ="data/AD_credibleSNP.rda")
 
 # "http://genome.ucsc.edu/cgi-bin/hgTables"
---
+++ R/Positionla_mapping_Promoters_1000Kb.R
@@ -0,0 +1,10 @@
+promoter <- read.table("data/Gencode19_promoter.bed")
+promoterranges <- GRanges(promoter[,1], IRanges(promoter[,2], promoter[,3]), gene = promoter[,4])
+# Overlap credible SNPs with promoter regions.
+olap <- findOverlaps(credranges, promoterranges)
+credpromoter <- credranges[queryHits(olap)]
+mcols(credpromoter) <- cbind(mcols(credpromoter), mcols(promoterranges[subjectHits(olap)]))
+
+gen_anno <- vroom::vroom("https://www.genenames.org/cgi-bin/download/custom?col=gd_hgnc_id&col=gd_app_sym&col=gd_app_name&col=gd_status&col=gd_prev_sym&col=gd_aliases&col=gd_pub_chrom_map&col=gd_pub_acc_ids&col=gd_pub_refseq_ids&col=gd_pub_ensembl_id&status=Approved&status=Entry%20Withdrawn&hgnc_dbtag=on&order_by=gd_hgnc_id&format=text&submit=submit")
+gen_anno <- gen_anno[!is.na(gen_anno$`Ensembl gene ID`), ]
+save(gen_anno, file = "data/Gen_Anno.rda")
