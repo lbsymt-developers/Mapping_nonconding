ec9fb17ec48421dde6c483ab82ba3788caf2117f
Erickcufe <erickcufe94@gmail.com>
2022-04-21 22:12:12 -0500

    forma para obtener los fasta

--- R/get_sequences_fasta.R
+++ R/get_sequences_fasta.R
@@ -1,7 +1,11 @@
-load("data/cred_promoters_GRanges.rda")
+load("data/promoter_ranges.rda")
 
+
+apoe <- promoterranges[promoterranges$gene_name %in% "APOE"]
+seqlevels(apoe) <- paste0("chr", seqlevels(apoe))
 library(BSgenome.Hsapiens.UCSC.hg38)
-seq <- BSgenome::getSeq(BSgenome.Hsapiens.UCSC.hg38, olaps)
+seq <- BSgenome::getSeq(BSgenome.Hsapiens.UCSC.hg38, apoe)
 # Aqui ponerle el nombre de los SNPs de donde provienen
 names(seq) <- paste0("SEQUENCE_", seq_along(seq))
 Biostrings::writeXStringSet(seq, "my.fasta")
+
