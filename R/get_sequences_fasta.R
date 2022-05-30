2bc8dae7a5a289d5b9c05f5f81d9a848cd426712
Erickcufe <erickcufe94@gmail.com>
2022-05-30 09:42:27 -0500

    .

--- R/get_sequences_fasta.R
+++ R/get_sequences_fasta.R
@@ -1,11 +1,12 @@
-load("data/promoter_ranges.rda")
+load("data/cred_promoters_GRanges.rda")
 
-
-apoe <- promoterranges[promoterranges$gene_name %in% "APOE"]
+library(GenomicRanges)
+apoe <- credpromoter[credpromoter$gene_name %in% "APOE"]
+apoe <- apoe[!duplicated(apoe$rsid)]
 seqlevels(apoe) <- paste0("chr", seqlevels(apoe))
 library(BSgenome.Hsapiens.UCSC.hg38)
 seq <- BSgenome::getSeq(BSgenome.Hsapiens.UCSC.hg38, apoe)
 # Aqui ponerle el nombre de los SNPs de donde provienen
 names(seq) <- paste0("SEQUENCE_", seq_along(seq))
-Biostrings::writeXStringSet(seq, "my.fasta")
+Biostrings::writeXStringSet(seq, "APOE_promoters.fasta")
 
