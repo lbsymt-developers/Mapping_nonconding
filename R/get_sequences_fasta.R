1aa3349f5dae5f2b870bcdacb65c3bcbe45311ae
Erickcufe <erickcufe94@gmail.com>
2022-05-30 18:20:56 -0500

    .

--- R/get_sequences_fasta.R
+++ R/get_sequences_fasta.R
@@ -1,16 +1,20 @@
 load("data/promoter_ranges.rda")
+load("resultados/cred_promoters_GRanges.rda")
 
 library(GenomicRanges)
-apoe <- promoterranges[promoterranges$gene_name %in% "APOE"]
-# apoe <- apoe[!duplicated(apoe$rsid)]
+apoe <- credpromoter[credpromoter$gene_name %in% "APOE"]
+apoe <- apoe[!duplicated(apoe$rsid)]
 seqlevels(apoe) <- paste0("chr", seqlevels(apoe))
 library(BSgenome.Hsapiens.UCSC.hg38)
-promoters_apoe <- GRanges(seqnames = "chr19",
-                          IRanges(start = c(44904790, 44904790, 44904790),
-                                  end = c(44905790, 44905790, 44905790),
-                                  names = c("APOE", "APOE", "APOE")))
-seq <- BSgenome::getSeq(BSgenome.Hsapiens.UCSC.hg38, apoe)
+promoters_apoe <- GRanges(seqnames = "chr19", IRanges(start= 44904790,
+                                                      end =44905790,
+                                                      name = "APOE"))
+seq <- BSgenome::getSeq(BSgenome.Hsapiens.UCSC.hg38, promoters_apoe)
 # Aqui ponerle el nombre de los SNPs de donde provienen
 names(seq) <- paste0("SEQUENCE_", seq_along(seq))
 Biostrings::writeXStringSet(seq, "APOE_promoters.fasta")
 
+subseq(seq, start = 558, end = 573)
+
+posi <- c(789, 581, 517)
+df$pos <- posi
